2b60ee30905dc1a65c8c3a1603e6fd5374b61d7d
Dmitriy <johndoe@email.com>
2024-12-13 12:02:34 +0300

    fix

--- Основы работы с Terraform/src/locals.tf
+++ Основы работы с Terraform/src/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  web_vm_name = "netology-develop-platform-web"
-  web_db_name = "netology-develop-platform-db"
+  web_vm_name = "${var.vm_web_name}"
+  web_db_name = "${var.vm_db_name}"
 }
